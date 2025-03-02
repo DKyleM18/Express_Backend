b14a04b04e2477328a2aafadfbc7e704fc8401ba
DKyleM18 <dkylem18@gmail.com>
2025-03-01 23:00:24 -0500

    adding news files

--- controllers/newsUsers.js
+++ controllers/newsUsers.js
@@ -1,40 +1,18 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const user = require("../models/user");
+const newsUser = require("../models/newsUser");
 const BadRequestError = require("../errors/bad-request-error");
 const UnauthorizedError = require("../errors/unauthorized-error");
 const NotFoundError = require("../errors/not-found-error");
 const ConflictError = require("../errors/conflict-error");
 const { JWT_SECRET } = require("../utils/config");
 
-const createUser = (req, res, next) => {
-  const { name, avatar, email, password } = req.body;
-
-  return bcrypt
-    .hash(password, 10)
-    .then((hash) => user.create({ name, avatar, email, password: hash }))
-    .then((user) => {
-      const userWithoutPassword = user.toObject();
-      delete userWithoutPassword.password;
-      res.status(201).send(userWithoutPassword);
-    })
-    .catch((err) => {
-      if (err.message === "Email in use" || err.code === 11000) {
-        next(new ConflictError(err.message));
-      } else if (err.name === "ValidationError" || err.name === "CastError") {
-        next(new BadRequestError(err.message));
-      } else {
-        next(err);
-      }
-    });
-};
-
 const createNewsUser = (req, res, next) => {
   const { name, email, password } = req.body;
 
   return bcrypt
     .hash(password, 10)
-    .then((hash) => User.create({ name, email, password: hash }))
+    .then((hash) => newsUser.create({ name, email, password: hash }))
     .then((user) => {
       const userWithoutPassword = user.toObject();
       delete userWithoutPassword.password;
@@ -58,7 +36,7 @@ const login = (req, res, next) => {
     throw new BadRequestError("Invalid data");
   }
 
-  return user
+  return newsUser
     .findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
@@ -77,7 +55,7 @@ const login = (req, res, next) => {
 
 const getCurrentUser = (req, res, next) => {
   const { _id } = req.user;
-  user
+  newsUser
     .findById(_id)
     .orFail()
     .then((user) => res.send(user))
@@ -92,36 +70,8 @@ const getCurrentUser = (req, res, next) => {
     });
 };
 
-const patchCurrentUser = (req, res, next) => {
-  const { _id } = req.user;
-  const { name, avatar } = req.body;
-  return user
-    .findByIdAndUpdate(
-      _id,
-      { name, avatar },
-      { new: true, runValidators: true }
-    )
-    .orFail()
-    .then((user) => res.send(user))
-    .catch((err) => {
-      console.error(err);
-      if (err.name === "ValidationError") {
-        next(new BadRequestError(err.message));
-      }
-      if (err.name === "DocumentNotFoundError") {
-        next(new NotFoundError(err.message));
-      }
-      if (err.name === "CastError") {
-        next(new BadRequestError(err.message));
-      }
-      next(err);
-    });
-};
-
 module.exports = {
-  createUser,
   getCurrentUser,
   login,
-  patchCurrentUser,
   createNewsUser,
 };
